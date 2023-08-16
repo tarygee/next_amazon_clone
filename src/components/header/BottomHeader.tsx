6efcd13f7278f16f70a7b743681fbd97983af2f3
tarygee <glorytarimo0680@gmail.com>
2023-08-16 11:19:15 +0300

    Next-auth, github provider,login

--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import {LuMenu} from 'react-icons/lu'
+import { useDispatch, useSelector } from 'react-redux';
+import { StateProps } from '../../../type';
+import { removeUser } from '@/store/nextSlice';
+import { signOut } from 'next-auth/react';
 
 const BottomHeader = () => {
+    const dispatch = useDispatch();
+    const {userInfo } = useSelector(
+        (state: StateProps) => state.next
+        );
+    const handleSignOut = () => {
+        signOut();
+        dispatch(removeUser());
+    };
   return (
     <div className='w-full h-10 bg-amazon_light text-sm text-white px-4 flex'>
         <p className='flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300'>
@@ -20,13 +32,17 @@ const BottomHeader = () => {
             Gift Cards
         </p>
         <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">
-            Sells
+            Sell
         </p>
-        <p className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300">
+        {userInfo && (
+            < button 
+            onClick={handleSignOut}
+            className="hidden md:inline-flex items-center h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300">
             Sign Out
-        </p>
+        </button>
+        )}
         </div>
   );
 };
 
-export default BottomHeader
+export default BottomHeader;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect } from 'react'
 import logo from '../../images/logo.png'
 import Image from 'next/image';
 import {SlLocationPin} from 'react-icons/sl'
@@ -6,13 +7,29 @@ import { HiOutlineSearch } from 'react-icons/hi'
 import { BiCaretDown } from 'react-icons/bi'
 import cartIcon from '../../images/cartIcon.png'
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { StateProps } from '../../../type';
+import { useSession, signIn, signOut } from "next-auth/react"
+import { addUser } from '@/store/nextSlice';
 
 const Header = () => { 
-    const { productData, favoriteData } = useSelector(
+    const { data: session } = useSession()
+    const { productData, favoriteData, userInfo } = useSelector(
         (state: StateProps) => state.next
         );
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if(session){
+            dispatch(
+                addUser({
+                    name: session?.user?.name,
+                    email: session?.user?.email,
+                    image: session?.user?.image,
+                })
+            );
+        }
+    }, [dispatch, session]);
+
   return (
     <div className='w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50'>
        <div className='h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4'>
@@ -37,7 +54,7 @@ const Header = () => {
         {/* searchbar */}
         <div className='flex-1 h-10 hidden md:inline-flex items:center justify-between relative'>
               <input
-                className='w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border transparent outline-none focus-visible:border-amazon_yellow'
+                className='w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none focus-visible:border-amazon_yellow'
                 type='text' 
                 placeholder='serach an item' />
                 <span className='w-12 h-full bg-amazon_yellow text-black text-2xl flex items-center justify-center absolute right-0 rounded-tr-md'>
@@ -47,7 +64,24 @@ const Header = () => {
 
 
         {/* signin */}
-        <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
+        {
+            userInfo? (
+                <div 
+        className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
+            <img
+            src={userInfo.image}
+            alt='userImage'
+            className='w-8 h-8 rounded-full object-cover'
+            />
+            <div className='text-xs text-gray-100 flex flex-col justify-between'>
+                <p className='text-white font-semibold'>{userInfo.name}</p>
+                <p>{userInfo.email}</p>
+            </div>
+        </div>
+            ):(
+                <div 
+        onClick={() => signIn()}
+        className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
             <p>Hello, Sign In</p>
             <p className='text-white font-bold flex items-center'>
                 Account & Lists{" "}
@@ -56,6 +90,8 @@ const Header = () => {
                 </span>
             </p>
         </div>
+            )
+        }
 
         {/* favorite */}
         <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative">
