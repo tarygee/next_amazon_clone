19072af7de894449152cc7da4fd8f1aa801a4930
tarygee <glorytarimo0680@gmail.com>
2024-04-04 15:33:35 +0300

    half way completed Accounttype page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Banner from "@/components/Banner";
 import Products from "@/components/Products";
 import { ProductProps } from "../../type";
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import Header from "@/components/header/Header";
 
 interface Props{
   productData: ProductProps;
@@ -10,6 +11,7 @@ interface Props{
 export default function Home({ productData }: Props) {
   return (
     <main>
+      {/* <Header/> */}
       <div className="w-screen-2xl mx-auto">
         <Banner/>
         <div className="relative md:-mt020 lgl:-mt-32 xl:-mt-60 z-20 mb-20">
@@ -23,8 +25,8 @@ export default function Home({ productData }: Props) {
 // SSR for data fetching
 
 export const getServerSideProps = async() =>{
-  // const res = await fetch('https://fakestoreapiserver.reactbd.com/tech')
-  const res = await fetch('https://217e-197-250-63-217.ngrok-free.app/api/v1/products')
+  const res = await fetch('https://fakestoreapiserver.reactbd.com/tech')
+  // const res = await fetch('https://0135-197-186-7-63.ngrok-free.app/api/v1/products')
   const productData = await res.json();
   return { props: { productData } };
 };
